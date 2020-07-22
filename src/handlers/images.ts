a762167a383439bfb44a50be0906858b321a3be2
Yoav Navon <yanavon@uc.cl>
2020-07-22 07:42:17 -0400

    Error handling in image upload

--- src/handlers/images.ts
+++ src/handlers/images.ts
@@ -1,21 +1,26 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { S3 } from 'aws-sdk';
 import { v1 as uuid } from 'uuid';
+import { createErrorResponse } from '@src/handlers/utils';
 
 const s3 = new S3({ signatureVersion: 'v4' });
 
 export const upload: APIGatewayProxyHandler = async () => {
-  const Key = `${uuid()}.jpg`;
-  const url = s3.getSignedUrl('putObject', {
-    Bucket: 'dog-finder-images',
-    Key,
-    Expires: 10,
-  });
-  return Promise.resolve({
-    statusCode: 200,
-    body: JSON.stringify({
-      url,
-      imageLink: `https://dog-finder-images.s3.amazonaws.com/${Key}`,
-    }),
-  });
+  try {
+    const Key = `${uuid()}.jpg`;
+    const url = s3.getSignedUrl('putObject', {
+      Bucket: 'dog-finder-images',
+      Key,
+      Expires: 10,
+    });
+    return Promise.resolve({
+      statusCode: 200,
+      body: JSON.stringify({
+        url,
+        imageLink: `https://dog-finder-images.s3.amazonaws.com/${Key}`,
+      }),
+    });
+  } catch (error) {
+    return createErrorResponse(error);
+  }
 };
