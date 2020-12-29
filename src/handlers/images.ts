fb02d78192da07d2f6bce13a2016a8e867764864
Yoav Navon <yanavon@uc.cl>
2020-12-28 22:57:01 -0300

    Add modelApi url

--- src/handlers/images.ts
+++ src/handlers/images.ts
@@ -5,8 +5,8 @@ import { createOkResponse, createErrorResponse } from '@src/handlers/utils';
 import axios from 'axios';
 
 const s3 = new S3({ signatureVersion: 'v4' });
-// const modelApi = 'http://visual-embedding-api-dev.us-east-1.elasticbeanstalk.com';
-const modelApi = 'http://127.0.0.1:5000';
+const modelApi = 'http://visual-embedding-api-dev.us-east-1.elasticbeanstalk.com';
+// const modelApi = 'http://127.0.0.1:5000';
 
 export const upload: APIGatewayProxyHandler = async () => {
   try {
