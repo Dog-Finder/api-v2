da772a250d92ed03b576d8908b2e2a83530ef32d
Yoav Navon <yanavon@uc.cl>
2021-01-02 21:53:35 -0300

    Save vector on found dog creation

---
+++ src/common/vectors.ts
@@ -0,0 +1,8 @@
+import axios, { AxiosResponse } from 'axios';
+
+const modelApi = 'http://visual-embedding-api-dev.us-east-1.elasticbeanstalk.com';
+// const modelApi = 'http://127.0.0.1:5000';
+type vectorData = { imageLink: string, userId: string, entryId: string}
+export function vectorizeImage(data: vectorData): Promise<AxiosResponse> {
+  return axios.post(`${modelApi}/predict-save`, data);
+}
