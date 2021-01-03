a4426d93223c9237868500f9e2e063766344f7e6
Yoav Navon <yanavon@uc.cl>
2021-01-02 23:25:21 -0300

    Retrieve most similar dogs by image

--- src/common/vectors.ts
+++ src/common/vectors.ts
@@ -1,8 +1,13 @@
 import axios, { AxiosResponse } from 'axios';
 
-const modelApi = 'http://visual-embedding-api-dev.us-east-1.elasticbeanstalk.com';
-// const modelApi = 'http://127.0.0.1:5000';
+// const modelApi = 'http://visual-embedding-api-dev.us-east-1.elasticbeanstalk.com';
+const modelApi = 'http://127.0.0.1:5000';
 type vectorData = { imageLink: string, userId: string, entryId: string}
+
 export function vectorizeImage(data: vectorData): Promise<AxiosResponse> {
   return axios.post(`${modelApi}/predict-save`, data);
+}
+
+export function searchKNN(imageLink: string): Promise<AxiosResponse> {
+  return axios.post(`${modelApi}/search`, { url: imageLink });
 }
