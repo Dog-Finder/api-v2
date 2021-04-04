00678db53d9cf3e356a8b66594b840ff2df99e85
yoavnavon <yanavon@uc.cl>
2021-04-04 16:11:57 -0400

    Feat/image search (#11)
    * Fix search params
    
    * Add score on similar dogs
    
    * Only upload image of found dogs
    
    * Allow search of found and lost

--- src/common/vectors.ts
+++ src/common/vectors.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
-const modelApi = 'http://visual-embedding-api-dev.us-east-1.elasticbeanstalk.com';
+const modelApi = 'https://ws1cwmx9c3.execute-api.us-east-1.amazonaws.com/dev';
 // const modelApi = 'http://127.0.0.1:5000';
 type vectorData = { imageLink: string, userId: string, entryId: string}
 
@@ -8,6 +8,6 @@ export function vectorizeImage(data: vectorData): Promise<AxiosResponse> {
   return axios.post(`${modelApi}/predict-save`, data);
 }
 
-export function searchKNN(imageLink: string, entryId: string): Promise<AxiosResponse> {
-  return axios.post(`${modelApi}/search`, { imageLink, entryId });
+export function searchKNN(imageLink: string, index: string, size: number): Promise<AxiosResponse> {
+  return axios.post(`${modelApi}/search`, { url: imageLink, index, size });
 }
--- src/handlers/search.ts
+++ src/handlers/search.ts
@@ -9,19 +9,21 @@ import { searchKNN } from '@src/common/vectors';
 export const search: APIGatewayProxyHandler = async (event) => {
   try {
     const requestBody = JSON.parse(event.body);
-    const { entryId, imageLink } = requestBody;
-    const response = await searchKNN(imageLink, entryId);
+    const { imageLink, index } = requestBody;
+    const response = await searchKNN(imageLink, index, 5);
     const { result: { hits } } = response.data;
-    const toGet = hits.hits.map((hit) => Object.assign(new DogFinderObject(), {
+    const toGet: DogFinderObject[] = hits.hits.map((hit) => Object.assign(new DogFinderObject(), {
       id: hit._source['user-id'],
       entry: hit._source['entry-id'],
     }));
-    const items = [];
-    // eslint-disable-next-line no-restricted-syntax
-    for await (const item of dynamodb.batchGet(toGet)) {
-      items.push(item.notice);
-    }
-    return createOkResponse('list', items);
+    const items = await Promise.all(toGet.map((item) => dynamodb.get(item)));
+    const notices = items.map((item, i) => {
+      const { notice } = item;
+      notice.score = hits.hits[i]._score;
+      return notice;
+    });
+
+    return createOkResponse('list', notices);
   } catch (error) {
     return createErrorResponse(error);
   }
