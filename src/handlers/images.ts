6002fec9e396e121748263de729cb2b13e2d5b94
Yoav Navon <yanavon@uc.cl>
2021-04-04 16:39:03 -0400

    Remove unused variable

--- src/handlers/images.ts
+++ src/handlers/images.ts
@@ -4,10 +4,7 @@ import { v1 as uuid } from 'uuid';
 import { createOkResponse, createErrorResponse } from '@src/handlers/utils';
 import { vectorizeImage } from '@src/common/vectors';
 
-
 const s3 = new S3({ signatureVersion: 'v4' });
-const modelApi = 'http://visual-embedding-api-dev.us-east-1.elasticbeanstalk.com';
-// const modelApi = 'http://127.0.0.1:5000';
 
 export const upload: APIGatewayProxyHandler = async () => {
   try {
